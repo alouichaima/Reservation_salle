8820bdf49494afed4450aabb8017d8893e0e448a
amal <amalbenothmen9@gmail.com>
2024-04-14 12:47:57 +0100

    corriger la logique de conflit

--- controllers/reservationController.js
+++ controllers/reservationController.js
@@ -8,15 +8,19 @@ exports.reserveMeetingRoom = async (req, res) => {
 
   try {
     // Vérifier si la salle de réunion est disponible pour la plage horaire spécifiée
-    const conflictingReservation = await Reservation.findOne({
+    const conflictingReservations = await Reservation.find({
       meetingRoom: meetingRoomId,
       $or: [
-        { startTime: { $lt: endTimeUTC }, endTime: { $gt: startTimeUTC } },
-        { startTime: { $gte: startTimeUTC, $lte: endTimeUTC } },
+        { startTime: { $lt: endTimeUTC }, endTime: { $gt: startTimeUTC } }, // Vérification de chevauchement
+        { startTime: { $gte: startTimeUTC, $lte: endTimeUTC } }, // Vérification d'inclusion
       ],
     });
 
-    if (conflictingReservation) {
+    // Filtrer les réservations conflictuelles ayant un ID de salle de réunion différent
+    const conflictingReservationsWithDifferentRoomId = conflictingReservations.filter(reservation => reservation.meetingRoom && reservation.meetingRoom.toString() !== meetingRoomId);
+
+
+    if (conflictingReservationsWithDifferentRoomId.length > 0) {
       return res.status(400).json({ message: 'Conflit de réservation. La salle de réunion est déjà réservée pour cette plage horaire.' });
     }
 
@@ -28,6 +32,7 @@ exports.reserveMeetingRoom = async (req, res) => {
       endTime: endTimeUTC,
     });
 
+    // Sauvegarder la nouvelle réservation dans la base de données
     await newReservation.save();
 
     res.status(201).json({ message: 'Réservation réussie !', reservation: newReservation });
@@ -35,28 +40,8 @@ exports.reserveMeetingRoom = async (req, res) => {
     console.error('Erreur lors de la réservation :', err);
     res.status(500).json({ message: 'Erreur lors de la réservation.' });
   }
-  exports.getAllReservations = async (req, res) => {
-  try {
-    // Récupérer toutes les réservations depuis la base de données
-    const reservations = await Reservation.find();
-
-    // Formater les données pour qu'elles soient compatibles avec FullCalendar
-    const formattedReservations = reservations.map(reservation => {
-      return {
-        title: `Réservé `,
-        start: reservation.startTime,
-        end: reservation.endTime,
-        id: reservation._id
-      };
-    });
-
-    res.status(200).json(formattedReservations);
-  } catch (error) {
-    console.error('Erreur lors de la récupération des réservations :', error);
-    res.status(500).json({ message: 'Erreur lors de la récupération des réservations.' });
-  }
 };
-};
+
 exports.getAllReservations = async (req, res) => {
   try {
     // Récupérer toutes les réservations depuis la base de données
